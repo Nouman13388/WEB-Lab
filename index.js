87f4d642c388053a7550453a9b83f333f28429fb
Nouman13388 <SP22-BSE-103@CUILAHORE.EDU.PK>
2023-10-07 20:48:38 +0500

    add/remove border

---
+++ index.js
@@ -0,0 +1,58 @@
+const person = {
+    firstName : "Sinclare",
+    lastName : "Henry",
+    fullName : function(){
+        return this.firstName + " " + this.lastName
+    }
+}
+
+// let complexArray = [
+//     [
+//       {
+//         one: 1,
+//         two: 2
+//       },
+//       {
+//         three: 3,
+//         four: 4
+//       }
+//     ],
+//     [
+//       {
+//         a: "a",
+//         b: "b"
+//       },
+//       {
+//         c: "c",
+//         d: "d"
+//       }
+//     ]
+//   ];
+
+// const parent = document.getElementById('parent')
+// const child = document.getElementById('child')
+// function removeFunction(){
+//     parent.removeChild(child)
+// }
+
+// const parent = document.getElementById('parent')
+// function createFunction(){
+//     const child = document.createElement('h1')
+//     child.innerHTML = "Hello World"
+//     parent.appendChild(child)
+// }
+
+const addB = document.getElementById('addB')
+function addBorder(){
+    const btn = document.getElementById('button')
+    if(addB.style.border=='4px solid black')
+    {
+        addB.style.border = "none"
+        btn.textContent = "Add Border"
+    }
+    else
+    {
+        console.log(addB.style.border = "4px solid black")
+        btn.textContent = "Remove Border"
+    }
+}
