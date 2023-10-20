f472b56926fb8f468a49c098592957aebb793959
Nouman13388 <SP22-BSE-103@CUILAHORE.EDU.PK>
2023-10-20 16:28:47 +0500

    lab task 20 oct

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const person = {
-    firstName : "Sinclare",
-    lastName : "Henry",
-    fullName : function(){
-        return this.firstName + " " + this.lastName
-    }
-}
+// const person = {
+//     firstName : "Sinclare",
+//     lastName : "Henry",
+//     fullName : function(){
+//         return this.firstName + " " + this.lastName
+//     }
+// }
 
 // let complexArray = [
 //     [
@@ -42,17 +42,50 @@ const person = {
 //     parent.appendChild(child)
 // }
 
-const addB = document.getElementById('addB')
-function addBorder(){
-    const btn = document.getElementById('button')
-    if(addB.style.border=='4px solid black')
-    {
-        addB.style.border = "none"
-        btn.textContent = "Add Border"
-    }
-    else
-    {
-        console.log(addB.style.border = "4px solid black")
-        btn.textContent = "Remove Border"
+// const addB = document.getElementById('addB')
+// function addBorder(){
+//     const btn = document.getElementById('button')
+//     if(addB.style.border=='4px solid black')
+//     {
+//         addB.style.border = "none"
+//         btn.textContent = "Add Border"
+//     }
+//     else
+//     {
+//         console.log(addB.style.border = "4px solid black")
+//         btn.textContent = "Remove Border"
+//     }
+// }
+
+var child = document.getElementById('1')
+const parent = document.getElementById('par')
+if(child==parent.firstElementChild){
+    document.getElementById('preBtn').style.display = "none"
+}
+
+function forward(){
+    child.classList.add('fill')
+    child = child.nextElementSibling
+    child.classList.add('fill')
+    child = child.nextElementSibling
+    document.getElementById('preBtn').style.display = "block"
+    if(child==parent.lastElementChild){
+        document.getElementById('nxtBtn').style.display = "none"
+        child.classList.add('fill')
     }
 }
+
+function previous(){
+    if(child==parent.lastElementChild){
+        document.getElementById('nxtBtn').style.display = "block"
+        child.classList.remove('fill')
+    }
+    child = child.previousElementSibling
+    child.classList.remove('fill')
+    child = child.previousElementSibling
+    child.classList.remove('fill')
+    if(child==parent.firstElementChild){
+        document.getElementById('preBtn').style.display = "none"
+    }
+}
+
