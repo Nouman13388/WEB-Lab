948128ce6989a1d7c7aaab8f38d1b2a7b6db03c5
Nouman13388 <SP22-BSE-103@CUILAHORE.EDU.PK>
2023-11-17 20:36:32 +0500

    practice

--- index.js
+++ index.js
@@ -57,10 +57,6 @@
 //     }
 // }
 
-
-
-
-
 // var child = document.getElementById('1')
 // const parent = document.getElementById('par')
 // if(child==parent.firstElementChild){
@@ -93,77 +89,127 @@
 //     }
 // }
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   const addButton = document.getElementById("submitBtn");
+//   const container = document.querySelector(".faq-container");
 
+//   // Function to toggle the "active" class
+//   function toggleActiveState(event) {
+//     const card = event.currentTarget;
+//     card.classList.toggle("active");
+//   }
 
+//   // Function to remove a card
+//   function removeCard(event) {
+//     const crossIcon = event.currentTarget;
+//     const card = crossIcon.parentElement.parentElement; // Get the parent of the parent
+//     card.remove();
+//   }
 
+//   // Add a click event listener to each card
+//   const cards = document.querySelectorAll(".faq");
+//   cards.forEach((card) => {
+//     card.addEventListener("click", toggleActiveState);
 
-document.addEventListener("DOMContentLoaded", function () {
-    const addButton = document.getElementById("submitBtn");
-    const container = document.querySelector(".faq-container");
-  
-    // Function to toggle the "active" class
-    function toggleActiveState(event) {
-        const card = event.currentTarget;
-        card.classList.toggle("active");
+//     // Add a click event listener to the cross icon to remove the card
+//     const crossIcon = card.querySelector(".fa-times");
+//     crossIcon.addEventListener("click", removeCard);
+//   });
+
+//   addButton.addEventListener("click", function () {
+//     const newTitleInput = document.getElementById("faqTitle");
+//     const newAnswerInput = document.getElementById("faqText");
+
+//     const newTitle = newTitleInput.value;
+//     const newAnswer = newAnswerInput.value;
+
+//     if (newTitle && newAnswer) {
+//       const newCard = document.createElement("div");
+//       newCard.className = "faq";
+//       newCard.innerHTML = `
+//                 <h3 class="faq-title">${newTitle}</h3>
+//                 <p class="faq-text">${newAnswer}</p>
+//                 <button class="faq-toggle">
+//                     <i class="fas fa-chevron-down"></i>
+//                     <i class="fas fa-times"></i>
+//                 </button>
+//             `;
+
+//       container.insertBefore(newCard, document.querySelector(".new-question"));
+//       newTitleInput.value = "";
+//       newAnswerInput.value = "";
+
+//       // Add a click event listener to the new card
+//       newCard.addEventListener("click", toggleActiveState);
+
+//       // Add a click event listener to the cross icon to remove the card
+//       const crossIcon = newCard.querySelector(".fa-times");
+//       crossIcon.addEventListener("click", removeCard);
+//     }
+//   });
+// });
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
+// function callBack(num1, num2, callback) {
+//   return callback(num1, num2);
+// }
+
+// console.log(callBack(10, 3, add));
+
+// const data = () => {
+//   return fetch(
+//     "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/",
+//     {
+//       method: "GET",
+//       headers: {
+//         "Content-type": "application/json; charset=UTF-8",
+//       },
+//     }
+//   )
+//     .then((response) => {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//       return Promise.reject(response);
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching data:", error);
+//       throw error;
+//     });
+// };
+
+// console.log(data());
+
+const postData = (user, score) => {
+  return fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        user: user,
+        score: score,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
     }
-  
-    // Function to remove a card
-    function removeCard(event) {
-        const crossIcon = event.currentTarget;
-        const card = crossIcon.parentElement.parentElement; // Get the parent of the parent
-        card.remove();
-    }
-  
-    // Add a click event listener to each card
-    const cards = document.querySelectorAll(".faq");
-    cards.forEach((card) => {
-        card.addEventListener("click", toggleActiveState);
-  
-        // Add a click event listener to the cross icon to remove the card
-        const crossIcon = card.querySelector(".fa-times");
-        crossIcon.addEventListener("click", removeCard);
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return Promise.reject(response);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      throw error;
+    })
+    .then((data) => {
+      console.log(data);
     });
-  
-    addButton.addEventListener("click", function () {
-        const newTitleInput = document.getElementById("faqTitle");
-        const newAnswerInput = document.getElementById("faqText");
-  
-        const newTitle = newTitleInput.value;
-        const newAnswer = newAnswerInput.value;
-  
-        if (newTitle && newAnswer) {
-            const newCard = document.createElement("div");
-            newCard.className = "faq";
-            newCard.innerHTML = `
-                <h3 class="faq-title">${newTitle}</h3>
-                <p class="faq-text">${newAnswer}</p>
-                <button class="faq-toggle">
-                    <i class="fas fa-chevron-down"></i>
-                    <i class="fas fa-times"></i>
-                </button>
-            `;
-  
-            container.insertBefore(newCard, document.querySelector(".new-question"));
-            newTitleInput.value = "";
-            newAnswerInput.value = "";
-  
-            // Add a click event listener to the new card
-            newCard.addEventListener("click", toggleActiveState);
-  
-            // Add a click event listener to the cross icon to remove the card
-            const crossIcon = newCard.querySelector(".fa-times");
-            crossIcon.addEventListener("click", removeCard);
-        }
-    });
-  });
+};
 
-
-  function add(num1, num2) {
-    return num1 + num2;
-  }
-
-  function callBack(num1, num2, callback) {
-    return callback(num1, num2);
-  }
-
-  console.log(callBack(10, 3, add));
+postData("Sinclare", 100);
